88bdacdbb20cf19477063cc076f7cbb359ebbcd0
uijeong <uijeong0525@gmail.com>
2024-09-11 02:14:25 +0900

    num1, 연산자, num2 순으로 나오도록 흐름을 수정하고, while문에 checking값 줘서 반복문을 제어하도록 함. switch문을 하나 더 만들어서 종료, 삭제, 계산을 선택할 수 있게 함. 결과값을 삭제 할 수 있는 코드를 수정함.

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -7,54 +7,60 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
-        ArrayList<Integer> intList = new ArrayList<Integer>();
+        boolean checking = true;
+        int num1 = 0;
+        int num2 = 0;
 
-
-        while (true) {
-            int result = 0;
+        while (checking) {  //checking으로 반복문 제어
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+            try {
+                num1 = sc.nextInt();
+                if (num1 < 0) {
+                    System.out.println("제대로된 숫자를 입력해주세요");  //음수를 입력받았을때
+                    checking = false;  //checking에 false을 넣어서 반복문 종료
+                }
+            } catch (Exception e) {
+                System.out.println("제대로된 숫자를 입력해주세요");  //숫자가 아닌 값을 입력 받았을 때
+                checking = false;
+            }
+
             System.out.print("사칙연산(+,-,*,/) 기호를 입력하세요: ");
             char operator = sc.next().charAt(0);
 
-
-            calculator.calculate(operator, num1, num2);
-
-
-
-//            switch (operator) {
-//                case '+':
-//                    result = num1 + num2;
-//                    break;
-//                case '-':
-//                    result = num1 - num2;
-//                    break;
-//                case '*':
-//                    result = num1 * num2;
-//                    break;
-//                case '/':
-//                    if (num2 == 0) {
-//                        System.out.println("분모는 0이 될 수 없습니다. 다시 입력하세요.");
-//                        continue;
-//                    }
-//                    result = num1 / num2;
-//                    break;
-//                default:
-//                    System.out.println("다시 입력해주세요.");
-//            }
-//            System.out.println("계산 결과는 : " + result);
-//
-//            intList.add(result);
-//            System.out.println("계산 결과 값 : " + intList.toString());
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            num2 = 0;
+            try {
+                num2 = sc.nextInt();
+                if (num2 < 0) {
+                    System.out.println("제대로된 숫자를 입력해주세요");   //음수를 입력받았을때
+                    checking = false;
+                }
+            } catch (Exception e) {
+                System.out.println("제대로된 숫자를 입력해주세요");   //숫자가 아닌 값을 입력 받았을 때
+                checking = false;
+            }
 
 
-            System.out.println("더 계산하시겠습니까? 아무키나 누르세요. (exit 입력 시 종료)");
-            String input = sc.next();
-            if (input.equals("exit")) {
-                System.out.println("계산기가 종료되었습니다.");
-                break;
+            calculator.calculate(operator, num1, num2);       // 계산
+
+            System.out.println("(1.종료) (2.삭제) (3.계산) 선택하십시오.");   //종료,삭제,계산 선택
+            String check = sc.next();
+            switch (check) {
+                case "1":   //1 입력시 checking에 false을 넣고 break로 스위치와 반복문 종료
+                    checking = false;
+                    break;
+
+                case "2":   //2 입력시 결과값 배열 보여주고 삭제 할 값 입력시 한개만 삭제
+                    System.out.print("삭제할 값을 입력하세요.");
+                    calculator.getResult();
+                    int delnum = Integer.parseInt(sc.next());
+                    calculator.delete(delnum);
+                    calculator.getResult();
+                    System.out.println("(1.종료) (2.삭제) (3.계산) 선택하십시오.");
+                    check = sc.next();  //종료할지 삭제할지 계산할지 선택 할 수 있게 함.
+
+                case "3": //3 입력시 계속 반복문 실행
+                    continue;
             }
 
         }
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -3,45 +3,53 @@ package com.example.calculator;
 import java.util.ArrayList;
 
 public class Calculator {
-    ArrayList<Integer> intList = new ArrayList<Integer>();
+    private ArrayList<Integer> resultList = new ArrayList<Integer>();
     private int result;
 
-    public void calculate(char operator, int num1, int num2) {
 
+    public void calculate(char operator, int num1, int num2) {
         switch (operator) {
             case '+':
                 result = num1 + num2;
+                resultList.add(result);
                 break;
             case '-':
                 result = num1 - num2;
+                resultList.add(result);
                 break;
             case '*':
                 result = num1 * num2;
+                resultList.add(result);
                 break;
             case '/':
                 if (num2 == 0) {
                     System.out.println("분모는 0이 될 수 없습니다. 다시 입력하세요.");
                 }
                 result = num1 / num2;
+                resultList.add(result);
                 break;
             default:
                 System.out.println("다시 입력해주세요.");
         }
         System.out.println("계산 결과는 : " + result);
-
-        intList.add(result);
-        System.out.println("이전 결과 값 : " + intList.toString());
-
-
+        System.out.println("이전 결과 값 : " + resultList.toString());
     }
 
+    //결과 값 저장
+    public void getResult() {
+        System.out.println(resultList.toString());
+    }
 
-    public int getResult() {
-        return result;
+    //입력받은 값 삭제
+    public void delete(int delnum) {
+        if (resultList.contains(delnum)) {
+            resultList.remove(Integer.valueOf(delnum));
+            System.out.print(delnum + "을 지웠습니다.");
+            resultList.toString();
+        } else {
+            System.out.println(delnum + "해당 값이 없습니다.");
+            resultList.toString();
+        }
     }
-    public void setName(String name) {
-        this.result = result;
-    }
-    /* Getter 메서드 구현 */
-    /* Setter 메서드 구현 */
+
 }
